201b421f28dc7c74cd1c9d2090c3ead49751e0e6
Harry.Liu <donote@163.com>
2018-11-14 10:47:46 +0800

    [fix] modify script tinily

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 # train 
 CUDA_VISIBLE_DEVICES=1  python examples/sample.py --train_path data/unify/train/data.txt --dev_path data/unify/dev/data.txt --expt_dir ./models/unify/
 
+CUDA_VISIBLE_DEVICES=1  python examples/sample.py --train_path data/bzdx/train/data.txt --dev_path data/bzdx/dev/data.txt --expt_dir ./models/bzdx/
+
 # test
 CUDA_VISIBLE_DEVICES=1 python examples/sample.py --test_path data/unify/test/data.txt --load_checkpoint 2018_11_13_15_39_16-epoch-79 --expt_dir ./models/unify/
 
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -5,5 +5,5 @@
 #python scripts/generate_data.py --output_dir data/kbs --input_file data_raw/kbs_llt.txt
 
 # For unify data
-python scripts/generate_data.py --output_dir ../data/unify --input_file ../data_raw/unify_ptjb.txt
+python scripts/generate_data.py --output_dir data/unify --input_file data_raw/unify_ptjb.txt
 
